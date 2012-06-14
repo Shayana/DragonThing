6f7545e1a20a8174ee5d5179368393285dc37ede
Vianney Vienne <shayana@r0x.fr>
2012-06-15 01:21:02 +0200

    "fuck this"

--- src/fr/Shayana/dragonThing/Commands.java
+++ src/fr/Shayana/dragonThing/Commands.java
@@ -9,11 +9,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 
 
-public class Commands {
+
+public class Commands{
 	protected Main plugin;
 	protected Dragon dragon;
 	public Commands(Main plugin){
 		this.plugin = plugin;
+		
 	}
 	
 	
@@ -24,7 +26,7 @@ public class Commands {
 		if (label.equalsIgnoreCase("dragon")){
 			Player p = (Player) sender;
 			WorldServer world = ((CraftWorld) p.getWorld()).getHandle();
-			dragon = new Dragon(p, world);
+			dragon = new Dragon(p.getLocation(), world);
 			world.addEntity(dragon);
 		}
 		return true;
--- src/fr/Shayana/dragonThing/Dragon.java
+++ src/fr/Shayana/dragonThing/Dragon.java
@@ -6,114 +6,168 @@ import net.minecraft.server.EntityComplexPart;
 import net.minecraft.server.EntityEnderDragon;
 import net.minecraft.server.World;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
 
 
 public class Dragon extends EntityEnderDragon{
 
 	Entity dragon = this.getBukkitEntity();
+	org.bukkit.World bukkitWorld;
+	
+	//Object location
+	private double locX;
+	private double locY;
+	private double locZ;
 
-	// Player Location
-	
-	private double pX;
-	private double pY;
-	private double pZ;
-
-	// Eye Location
-	
-	private double eX;
-	private double eY;
-	private double eZ;
-	
 	// Dragon Start
 	
-	private double sX;
-	private double sY;
-	private double sZ;
+	private double startX;
+	private double startY;
+	private double startZ;
+
 	
-	// Dragon Target
+	//Where dragon flies down and up
+
+	private double downY;
 	
-	private double dX;
-	private double dZ;
+	private double upX;
 	
-	// Dragon move points
+	// Dragon's move points
 	
 	private double mX;
 	private double mZ;
 	private double mY;
 	private double distanceX;
 	private double distanceZ;
+	private double distanceY;
+	
+	//Dragon's travel end
+	
+	private double lastX;
+	private double lastZ;
+	private double lastY = 150;
+	private Location target;
+	//Dragon location
+	
+	private double currentX; 
+	private double currentZ; 
+	private double currentY; 
 	
 	
 
-	public Dragon(Player p, World world){
+	public Dragon(Location loc, World world){
 		super(world);
 		this.children = new EntityComplexPart[] { this.g = new EntityComplexPart(this, "head", 6.0F, 6.0F),  this.h = new EntityComplexPart(this, "body", 8.0F, 8.0F), this.i = new EntityComplexPart(this, "tail", 4.0F, 4.0F), this.j = new EntityComplexPart(this, "tail", 4.0F, 4.0F), this.k = new EntityComplexPart(this, "tail", 4.0F, 4.0F), this.l = new EntityComplexPart(this, "wing", 4.0F, 4.0F), this.m = new EntityComplexPart(this, "wing", 4.0F, 4.0F) };
 
-		//Player position
+		//Get the objet location
+		this.locX = loc.getX();
+		this.locY = loc.getY();
+		this.locZ = loc.getZ();
 		
-		pX = p.getLocation().getX();
-		pY = p.getLocation().getY();
-		pZ = p.getLocation().getZ();
+		//Set up start Location
 		
-		// Player's eye direction
+		this.bukkitWorld = loc.getWorld();
+		this.startX = loc.getX() - 30;
+		this.startZ = loc.getZ();
+		this.startY = 130; //Clouds are at 127
+		setPosition(startX, startY, startZ);
+		//Set up stop flying down height
 		
-		eX = p.getTargetBlock(null, 100).getX();
-		eY = p.getTargetBlock(null, 100).getY();
-		eZ = p.getTargetBlock(null, 100).getZ();
+		this.downY = loc.getY() + 20;
 		
-		// Dragon start Location 
-		sX = pX;
-		sY = pY + 20;
-		sZ = pZ;
-		setPosition(sX, sY , sZ);
+		//Set up flying up location
+		
+		this.upX = loc.getX() + 20;
+		
+		//Set up remove points
+		
+		this.lastX = loc.getX() + 40;
+		this.lastZ = loc.getZ();
+		this.target = new Location(bukkitWorld, lastX, lastY, lastZ);
 	}
+	
+	
+	
 		
-			
+		
 
+//The dragon flies straight
+	public void fly(){
 
-	public void setMove(){
-		
-		
-		if (eX > pX){
-			dX = pX + 25;
-		}
-		if (eX < pX){
-			dX = pX - 25;
-		}
-		if (eX == pX){
-			dX = pX;
-		}
-		if (eZ > pZ){
-			dZ = pZ + 25;
-		}
-		if (eY < pZ){
-			dZ = pZ - 25;
-		}
-		if (eZ == pZ){
-			dZ = pZ;
-		}
-		
-		
-		distanceX = pX - dX;
-        distanceZ = pZ - dZ;
+		distanceX = lastX - currentX;
+        distanceZ = lastZ - currentZ;
         
-        double tick = Math.sqrt((distanceX * distanceX) + 0 + (distanceZ * distanceZ)) / 2;
+        double tick = Math.sqrt((distanceX * distanceX) + 0 + (distanceZ * distanceZ));
         mX = Math.abs(distanceX) / tick;
         mZ = Math.abs(distanceZ) / tick;
-        mY = sY;
+        mY = currentY;
+       
     }
 		
+
 	
-	
-	
-	
-	@Override
-	public void e(){
-		 setMove();
-		 setPosition(mX, mY, mZ);
+//The dragon flies down from the clouds
+	public void flyDown(){
+		
+		distanceX = lastX - currentX ;
+        distanceZ = lastZ - currentZ;
+        distanceY = downY - currentY;
+        double tick = Math.sqrt((distanceX * distanceX) + (distanceY * distanceY) + (distanceZ * distanceZ));
+        mX = Math.abs(distanceX) / tick;
+        mZ = Math.abs(distanceZ) / tick;
+        mY = Math.abs(distanceY) / tick;
+        
+
 	}
+	
+	
+//The dragon flies up to the clouds
+	public void flyUp(){
+		distanceX = lastX - currentX;
+        distanceZ = lastZ - currentZ;
+        distanceY = lastY - currentY;
+        double tick = Math.sqrt((distanceX * distanceX) + (distanceY * distanceY) + (distanceZ * distanceZ));
+        mX = Math.abs(distanceX) / tick;
+        mZ = Math.abs(distanceZ) / tick;
+        mY = Math.abs(distanceY) / tick;
+
+	}
+	
+
+//What happens each server tick
+		@Override
+	public void e(){
+		Bukkit.getServer().broadcastMessage(dragon.getLocation().toString());
+		currentX = dragon.getLocation().getX();
+		currentZ = dragon.getLocation().getZ();
+		currentY = dragon.getLocation().getY();
+		//The dragon is at the right height to disappear, and has made its travel allready
+		if (currentY == lastY && currentX > locX){
+			dragon.remove();
+		}
+		
+		//The dragon is higher than the Y position where he's got to fly and has not done its travel
+		if (currentY > downY && currentX < upX ){
+			flyDown();
+		}
+		
+		//The dragon is at the X position where he has to fly up
+		if (currentX >= upX){
+			flyUp();
+		}
+		else{
+			fly();
+		}
+		Location mLoc = new Location(bukkitWorld, mX, mY, mZ);
+		dragon.setVelocity(mLoc.toVector());
+        setPosition(mX, mY, mZ);
+        return;
+		 
+	}
+	
+	
 	
 
 
